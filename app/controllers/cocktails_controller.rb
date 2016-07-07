c4c0ff676401b02cdbd88f943c5590eb3ebcd4a9
Antoine Reveau <antoinerev@gmail.com>
2016-07-07 16:49:07 +0200

    controllers new implemented for doses and cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,19 +1,23 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
+
   end
 
   def new
     @cocktail = Cocktail.new()
+    # @dose = Dose.new(cocktail_id: @cocktail.id)
   end
 
   def create
     @cocktail = Cocktail.new(params_cocktail)
     if @cocktail.save
-      redirect_to cocktail_path
+      redirect_to cocktail_path(@cocktail.id)
     else
       render :new
     end
@@ -23,5 +27,9 @@ class CocktailsController < ApplicationController
 
   def params_cocktail
     params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,36 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+
   def new
+    @dose = Dose.new()
   end
 
   def create
+    # @ingredient =
+    @dose = Dose.new(
+      description: params_dose[:description],
+      ingredient_id: params_dose[:ingredient_id],
+      cocktail_id: @cocktail.id,
+      )
+    if @dose.save
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      render :new
+    end
+
   end
 
   def detroy
   end
+
+  private
+
+  def params_dose
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
 end
