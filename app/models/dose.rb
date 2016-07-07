46f359a049ec34138e0931dd6ee4507f5ec11490
Antoine Reveau <antoinerev@gmail.com>
2016-07-07 12:17:16 +0200

    validation added

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail_id
   belongs_to :ingredient_id
+
+  validates :description, :dose, :ingredient, presence: true
+  validates :ingredient uniqueness: { scope: :cocktail }
 end
